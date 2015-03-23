277cd85e81679c97ee41672a523dd686eeed1af4
Renan Azevedo <renan.henri08@hotmail.com>
2015-03-23 19:16:47 -0300

    Estilo tela login pronto

--- blog/login.php
+++ blog/login.php
@@ -2,22 +2,32 @@
 <html>
 	<head>
 		<title>Renanblog</title>
-		<link rel="stylesheet" href = "CSS/style.css">
+		<link rel="stylesheet" href = "CSS/styleLogin.css">
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
 	</head>
 	<body>
-		<div>
-			<form method="post" action="confirmacao.php">
-				<fieldset>
-					<legend>Login</legend>
-						E-mail: <input type = "text" name = "email">
-						Senha: <input type = "password" name = "senha">
-						<input type = "submit" value = "Entrar" name = "entrar">
-				</fieldset>
-			</form>
+	
+		<div id = box>
+		
+			<div id = "cadastro">
+				<form method="post" action="confirmacao.php">
+					<fieldset>
+						<legend class = "borda">Login</legend>
+							E-mail: <input type = "text" name = "email">
+							Senha: <input type = "password" name = "senha">
+							<input type = "submit" value = "Entrar" name = "entrar" id = "botao">
+					</fieldset>
+				</form>
+			</div>
+			
+			<div class = "cadastrar">
+				<a href="cadastro.php">Cadastrar</a>
+			</div> 
+			
+			<div class = "visitante">
+				<a href="index.php?admin=0">Entrar como visitante</a>
+			</div>
 		</div>
-		<div id="visitante">
-			<a href="index.php?admin=0">Entrar como visitante</a>
-		</div>
+		
 	</body>
 </html>
